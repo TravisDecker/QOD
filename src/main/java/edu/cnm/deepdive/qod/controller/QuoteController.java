74199496f61cfdae8ab92846272add0db84af453
Travis Decker <decker4693@gmail.com>
2018-11-08 16:26:02 -0700

    Added source controller, view objects ect..

--- src/main/java/edu/cnm/deepdive/qod/controller/QuoteController.java
+++ src/main/java/edu/cnm/deepdive/qod/controller/QuoteController.java
@@ -1,26 +1,24 @@
 package edu.cnm.deepdive.qod.controller;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import edu.cnm.deepdive.qod.model.dao.QuoteRepository;
 import edu.cnm.deepdive.qod.model.entity.Quote;
+import edu.cnm.deepdive.qod.view.Nested;
 import java.util.List;
 import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.ExposesResourceFor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@ExposesResourceFor(Quote.class)
+
 @RestController
 @RequestMapping("/quotes")
 public class QuoteController {
@@ -32,58 +30,45 @@ public class QuoteController {
     this.quoteRepository = quoteRepository;
   }
 
+  @JsonView(Nested.class)
   @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
   public List<Quote> list() {
     return quoteRepository.findAllByOrderByText();
   }
 
-  @GetMapping(value = "search", produces = MediaType.APPLICATION_JSON_VALUE)
-  public List<Quote> search(@RequestParam(value = "text") String text,
-      @RequestParam(value = "source")String source ) {
-    List<Quote> response = null;
-    if (text != null && source == null) {
-      response = quoteRepository.findAllByTextContainingOrderByText(text);
-    } else if (text == null && source != null) {
-      response = quoteRepository.findAllByTextContainingOrderByText(source);
-    } else if (text != null && source != null) {
-      response = quoteRepository.findAllByTextContainingAndSourceContainingOrderBySource(text, source);
-    }
-    return response;
-  }
-  //TODO use default value, set reqired to false, use a switch, handle both null or default.
+//  @GetMapping(value = "search", produces = MediaType.APPLICATION_JSON_VALUE)
+//  public List<Quote> search(@RequestParam(value = "text", required = false) String text,
+//      @RequestParam(value = "source", required = false) String source) {
+//    if (text == null && source == null) {
+//      return list();
+//    }
+//    if (text == null) {
+//      return quoteRepository.findAllBySourceContainingOrderBySourceAscTextAsc(source);
+//    }
+//    if (source == null) {
+//      return quoteRepository.findAllByTextContainingOrderByText(text);
+//    }
+//    return quoteRepository.findAllBySourceContainingAndTextContainingOrderBySourceAscTextAsc(source, text);
+//  }
 
-  @GetMapping(value = "searchByText", produces = MediaType.APPLICATION_JSON_VALUE)
-  public List<Quote> searchText(@RequestParam("fragment") String fragment) {
-    return quoteRepository.findAllByTextContainingOrderByText(fragment);
-  }
-
-  @GetMapping(value = "searchBySource", produces = MediaType.APPLICATION_JSON_VALUE)
-  public List<Quote> searchSource(@RequestParam("fragment") String fragment) {
-    return quoteRepository.findAllBySourceContainingOrderBySourceAscTextAsc(fragment);
-  }
-
+  @JsonView(Nested.class)
   @GetMapping(value = "{quoteId}", produces = MediaType.APPLICATION_JSON_VALUE)
   public Quote get(@PathVariable("quoteId") long quoteId) {
     return quoteRepository.findById(quoteId).get();
   }
 
+  @JsonView(Nested.class)
   @GetMapping(value = "random", produces = MediaType.APPLICATION_JSON_VALUE)
   public Quote random() {
-  return quoteRepository.findRandom().get();
+    return quoteRepository.findRandom().get();
   }
 
-  @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
-      produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<Quote> post(@RequestBody Quote quote) {
-    quoteRepository.save(quote);
-    return ResponseEntity.created(null).body(quote);
-  }
 
-  @DeleteMapping(value = "{quoteId}")
-  @ResponseStatus(HttpStatus.NO_CONTENT)
-  public void delete(@PathVariable("quoteId") long quoteId) {
-    quoteRepository.delete(get(quoteId));
-  }
+//  @DeleteMapping(value = "{quoteId}")
+//  @ResponseStatus(HttpStatus.NO_CONTENT)
+//  public void delete(@PathVariable("quoteId") long quoteId) {
+//    quoteRepository.delete(get(quoteId));
+//  }
 
   @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Quote not found")
   @ExceptionHandler(NoSuchElementException.class)
--- src/main/java/edu/cnm/deepdive/qod/model/dao/QuoteRepository.java
+++ src/main/java/edu/cnm/deepdive/qod/model/dao/QuoteRepository.java
@@ -1,6 +1,7 @@
 package edu.cnm.deepdive.qod.model.dao;
 
 import edu.cnm.deepdive.qod.model.entity.Quote;
+import edu.cnm.deepdive.qod.model.entity.Source;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.Query;
@@ -10,13 +11,19 @@ public interface QuoteRepository extends CrudRepository<Quote, Long> {
 
   List<Quote> findAllByTextContainingOrderByText(String fragment);
 
-  List<Quote> findAllByTextContainingAndSourceContainingOrderBySource(String text, String source);
-
   List<Quote> findAllByOrderByText();
 
   List<Quote> findAllBySourceContainingOrderBySourceAscTextAsc(String fragment);
 
-  @Query(value = "SELECT * FROM Quote ORDER By RANDOM() FETCH FIRST 1 ROW ONLY", nativeQuery = true)
+  List<Quote> findAllBySourceOrderByText(Source source);
+
+  Optional<Quote> findFirstBySourceAndId(Source source, long quoteId);
+
+//  List<Quote> findAllBySourceContainingAndTextContainingOrderBySourceAscTextAsc(
+//      String source, String text);
+
+  @Query(value = "SELECT * FROM Quote ORDER BY RANDOM() FETCH FIRST 1 ROW ONLY", nativeQuery = true)
   Optional<Quote> findRandom();
 
 }
+
